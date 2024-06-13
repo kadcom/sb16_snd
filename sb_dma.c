ca4dc446bd3133a107b71aa718c658889d6f90ac
Didiet Noor <lynxluna@gmail.com>
2024-06-13 12:18:57 +0700

    dma: use it as masks

--- sb_dma.c
+++ sb_dma.c
@@ -9,12 +9,12 @@
 #define DMA_ENABLE 0x00
 
 /* This bit field needs to be set for each DMA 
- * It's hard-coded now for DMA1
+ * It's hard-coded now for read mode, single cycle
  *
  * If you want to change DMA, please create
  * a proper function
  */
-#define DMA_SINGLE_CYCLE 0x49
+#define DMA_SINGLE_CYCLE 0x48
 
 #define SB_DMA_BUFFER_SIZE KB_16
 
